08a6a4f6c6fbbac009dbca65555b3598597a69b8
Lila <djyasin.ly@gmail.com>
2021-11-02 12:59:17 -0400

    Got everything installed and updated time zone.

---
+++ djangogirls/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/delialayasin/momentum-assignments/django-girls-tutorial-djyasin/djangogirls/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
